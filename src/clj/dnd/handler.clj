d5bbe5c19759100f5522795e601f228d38de4f3c
Joe Einertson <jeinerts@umm>
2018-04-15 15:29:01 -0500

    Compojure API [WIP]

--- src/clj/dnd/handler.clj
+++ src/clj/dnd/handler.clj
@@ -1,22 +1,37 @@
 (ns dnd.handler
-  (:require [compojure.core :refer [GET POST PUT PATCH DELETE defroutes]]
+  (:require [compojure.api.coercion.schema :as schema-coercion]
+            [compojure.api.sweet :as capi]
+            [compojure.core]
             [compojure.route :refer [resources]]
             [ring.middleware.cors :as cors]
-            [ring.middleware.json :as json]
             [ring.middleware.reload :as reload]
-            [ring.util.response :as response :refer [resource-response]]))
+            [ring.util.response :as response :refer [resource-response]]
+            [schema.core :as s]))
 
-(defroutes routes
-  (GET "/" [] (resource-response "index.html" {:root "public"}))
-  (POST "/roll-die" [:as {body :body}]
-    (let [{:keys [sides modifier]
-           :or   {modifier 0}}
-          body]
-      (println body)
-      (println (format "Rolling 1d%s+%s" sides modifier))
-      (response/response {:roll (+ (inc (rand-int sides))
-                                   modifier)})))
-  (resources "/"))
+(def routes
+  (capi/api
+   {:coercion (schema-coercion/create-coercion
+               (assoc-in
+                schema-coercion/default-options
+                [:response :default]
+                schema-coercion/json-coercion-matcher))
+    :swagger {:ui "/api-docs"
+              :spec "/swagger.json"
+              :data {:info {:title ""
+                            :description ""}}}}
+
+   (capi/GET "/" [] (resource-response "index.html" {:root "public"}))
+
+   (capi/context "/roll-die" []
+     (capi/POST "/" []
+       :body-params [sides     :- s/Int
+                     {modifier :- s/Int 0}]
+       :return {:roll s/Int}
+       (println (format "Rolling 1d%s+%s" sides modifier))
+       (response/response {:roll (+ (inc (rand-int sides))
+                                    modifier)})))
+
+   (resources "/")))
 
 (def dev-handler
   (-> #'routes
@@ -33,7 +48,5 @@
 (def handler
   (-> #'routes
       wrap-debug-info
-      json/wrap-json-response
-      (json/wrap-json-body {:keywords? true :bigdecimals? true})
       (cors/wrap-cors :access-control-allow-origin  [#".*"]
                       :access-control-allow-methods [:get :put :post :delete])))
