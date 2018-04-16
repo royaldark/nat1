fce1bd55d505ff17c58b5415667057574723bc01
Joe Einertson <jeinerts@umm>
2018-04-15 21:47:24 -0500

    stuff

--- src/cljs/dnd/views.cljs
+++ src/cljs/dnd/views.cljs
@@ -17,44 +17,58 @@
      :class "site-header"]))
 
 (defn roller []
-  (let [id       (random-uuid)
-        number   (reagent/atom "1")
-        sides    (reagent/atom "20")
-        modifier (reagent/atom "0")
-        digit-re #"^[0-9]+$"
-        result   (re-frame/subscribe [::subs/die-roll id])
+  (let [id         (random-uuid)
+        number     (reagent/atom "1")
+        sides      (reagent/atom "20")
+        modifier   (reagent/atom "0")
+        digit-re   #"^[0-9]+$"
+        result     (re-frame/subscribe [::subs/die-roll id])
         select-all (handler-fn
-                     (.select (.-target event)))]
+                     (.select (.-target event)))
+        text-style {:padding "0"
+                    :text-align :center}]
     (fn []
       [re-com/h-box
+       :align :end
+       :gap "0.25rem"
+       :padding "0 0 0 1rem"
        :children [[re-com/input-text
-                   :width "2.5rem"
+                   :width "1.5rem"
                    :model number
                    :validation-regex digit-re
                    :on-change #(reset! number %)
-                   :attr {:on-focus select-all}]
+                   :attr {:on-focus select-all}
+                   :style text-style]
                   "d"
                   [re-com/input-text
-                   :width "3.5rem"
+                   :width "2.5rem"
                    :model sides
                    :validation-regex digit-re
                    :on-change #(reset! sides %)
-                   :attr {:on-focus select-all}]
+                   :attr {:on-focus select-all}
+                   :style text-style]
                   "+"
                   [re-com/input-text
-                   :width "3.5rem"
+                   :width "2rem"
                    :model modifier
                    :validation-regex digit-re
                    :on-change #(reset! modifier %)
-                   :attr {:on-focus select-all}]
+                   :attr {:on-focus select-all}
+                   :style text-style]
                   "="
-                  (or @result "__")
+                  [re-com/box
+                   :child (if-let [num @result]
+                            (str num)
+                            "\u00A0")
+                   :style {:box-shadow "0 2px 0 0 blue"
+                           :min-width "2rem"}]
                   [re-com/button
                    :label "Roll"
                    :on-click #(re-frame/dispatch [::api/roll-die id
                                                   (js/parseInt @number)
                                                   (js/parseInt @sides)
-                                                  (js/parseInt @modifier)])]]])))
+                                                  (js/parseInt @modifier)])
+                   :style {:margin-left "1rem"}]]])))
 
 (defn home-panel []
   [re-com/v-box
